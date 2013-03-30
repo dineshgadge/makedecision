2bad82503a9874f479225d9884ff470a5a302473
Dinesh Gadge <dinesh.gadge@gmail.com>
2013-03-30 08:45:48 +0530

    Using let in specs

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,22 +4,29 @@ describe "StaticPages" do
 
   subject {page}
 
+  shared_examples_for "all static pages" do
+    it {should have_selector 'h1', text: heading}
+  end
+
   describe "Home Page" do
     before {visit root_path}
+    let(:heading) {'Make Decision'}
 
-    it {should have_selector 'h1', text: 'Make Decision'}
+    it_should_behave_like "all static pages"
     it {should have_selector 'title', text: 'Make Decision'}
   end
 
   describe "Help Page" do
     before {visit help_path}
+    let(:heading) {'Help'}
 
-    it {should have_selector 'h1', text: 'Help'}
+    it_should_behave_like "all static pages"
   end
 
   describe "About Page" do
     before {visit about_path}
+    let(:heading) {'About Us'}
 
-    it {should have_selector 'h1', text: 'About Us'}
+    it_should_behave_like "all static pages"
   end
 end
